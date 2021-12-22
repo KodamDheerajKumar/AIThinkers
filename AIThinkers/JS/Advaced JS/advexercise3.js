45cc0a2309148c93fcb6389687f325ac449136df
DheerajKumarKodam <58454961+KodamDheerajKumar@users.noreply.github.com>
2021-12-22 12:20:55 +0530

    Add files via upload

---
+++ AIThinkers/JS/Advaced JS/advexercise3.js
@@ -0,0 +1,94 @@
+// change everything below to the newer Javascript!
+
+// let + const
+var a = 'test';
+var b = true;
+var c = 789;
+a = 'test2';
+
+==>
+let a = 'test';
+const b = true;
+const c = 789;
+a = 'test2';
+
+
+
+// Destructuring
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+
+==>
+const {firstName,lastName,age,eyeColor}=person;
+
+
+// Object properties
+var a = 'test';
+var b = true;
+var c = 789;
+
+var okObj = {
+  a: a,
+  b: b,
+  c: c
+};
+
+==>
+var a = 'test';
+var b = true;
+var c = 789;
+
+var okObj = {a,b,c};
+
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+
+==>
+const message= `Hello ${firstName} have I met you before ? I think we met in ${city} last summer no???`
+
+
+// default arguments
+// default age to 10;
+function isValidAge(age) {
+    return age
+}
+
+==>
+const isValidAge = (age = 10) => age;
+
+
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+
+const sym =symbol('This is my first school');
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+
+==>
+const whereAmI = (username, location) => {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
